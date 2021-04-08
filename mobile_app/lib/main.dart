61bb17ba4434563de20d813affd3e481e588cdbb
Noé <noe-lam-anh.sechet@epitech.eu>
2021-04-08 20:40:22 +0200

    [ADD] riverpod dependencies

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:mobile_app/config/theme.dart';
 import 'package:mobile_app/config/routes.dart';
 import 'package:mobile_app/pages/auth/sign_in/sign_in.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
